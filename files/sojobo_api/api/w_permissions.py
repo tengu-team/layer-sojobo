e2d0f96fcad178227865efeaa5e620d2b82e31f9
Michiel <michielghyselinck@gmail.com>
2018-03-02 17:06:27 +0100

    authorize first steps

--- files/sojobo_api/api/w_permissions.py
+++ files/sojobo_api/api/w_permissions.py
@@ -1,4 +1,12 @@
-{
+#!/usr/bin/env python3
+
+"""
+This module has a static dictionary with each call and its required access levels.
+It is used to check if a user has the rights to perform certain actions.
+"""
+
+
+permissions = {
 	"/controllers/controller": {
 		"get": {
 			"c_access": ["tengu_admin", "company_admin", "superuser"]
@@ -67,5 +75,45 @@
 		"del": {
 			"m_access": ["admin", "write"]
 		}
+	},
+    "/users/user/controllers/controller": {
+		"get": {
+			"c_access": ["tengu_admin", "company_admin", "superuser"]
+		},
+        "put": {
+			"c_access": ["tengu_admin", "company_admin", "superuser"]
+		}
+	},
+    "/users/user/controllers/controller/models": {
+		"get": {
+			"c_access": ["tengu_admin", "company_admin", "superuser"]
+		},
+        "put": {
+			"c_access": ["tengu_admin", "company_admin", "superuser"]
+		}
+	},
+    "/users/user/controllers/controller/models/model": {
+		"get": {
+			"c_access": ["tengu_admin", "company_admin", "superuser"],
+            "m_access": ["admin"]
+		}
 	}
 }
+
+
+def c_authorize(controller_connection_info, resource, method):
+    controller_access = controller_connection_info['c_access']
+    try:
+        allowed_access_levels = permissions[resource][method]['c_access']
+        return controller_access in allowed_access_levels
+    except KeyError, e:
+        print("A KeyError has occured: {}".format(e))
+
+
+def m_authorize(model_connection_info, resource, method):
+    model_access = model_connection_info['m_access']
+    try:
+        allowed_access_levels = permissions[resource][method]['m_access']
+        return model_access in allowed_access_levels
+    except KeyError, e:
+        print("A KeyError has occured: {}".format(e))
