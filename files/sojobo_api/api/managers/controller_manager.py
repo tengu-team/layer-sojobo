b062a36fe275ac097d8dc4902133199763e32029
Michiel Ghyselinck <michielghyselinck@gmail.com>
2018-05-14 11:02:00 +0200

    Add controller object class.

---
+++ files/sojobo_api/api/managers/controller_manager.py
@@ -0,0 +1,14 @@
+
+
+class ControllerObject:
+    def __init__(self, name, state, type, region, models, endpoints, uuid,
+                 ca_cert, default_credential_name):
+        self.name = name
+        self.state = state
+        self.type = type
+        self.region = region
+        self.models = models
+        self.endpoints = endpoints
+        self.uuid = uuid
+        self.ca_cert = ca_cert
+        self.default_credential_name = default_credential_name
